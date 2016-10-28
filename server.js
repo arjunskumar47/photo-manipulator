1698ab411260c36965ef223965572bfa05c36eda
Arjun S Kumar <arjunskumar47@users.noreply.github.com>
2016-10-28 22:08:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var config = require('./config.js')
+var config = require('./config')
 var app = express()
 
 app.use(express.static(__dirname+'/public'))
-app.use(bodyParser({limit:'50mb'}))
-app.use(bodyParser.json())
+app.use(bodyParser.json({
+	limit:'50mb'
+}))
 
-require('./routes.js')(app)
+require('./routes')(app)
 
 app.listen(config.port,function() {
 	console.log('The app is listening on ',config.port)
